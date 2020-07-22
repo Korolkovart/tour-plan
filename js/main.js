de5fd423dc757eb52c149def817eb14347832194
Korolkovart <56297922+Korolkovart@users.noreply.github.com>
2020-07-22 19:08:53 +0300

    create: added swiper-slider + keyboard control

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var mySwiper = new Swiper('.swiper-container', {
+  // keyboard control
+  keyboard: {
+    enabled: true,
+  },
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
+  },
+});
